808345c16b6fdd470b16c27d8b9af41471ba56f6
halflifefan <37071910+halflifefan@users.noreply.github.com>
2021-02-10 03:43:37 +0600

    Create anim_layer.lua

---
+++ anim_layer.lua
@@ -0,0 +1,17 @@
+local entity = require 'entity'
+
+client.set_event_callback('setup_command', function()
+    local local_player = entity.get_local_player()
+
+    local anim_layer = local_player:get_anim_overlay(1)
+    if not anim_layer.entity then
+        return 
+    end
+    
+    local activity = local_player:get_sequence_activity(anim_layer.sequence)
+    if activity == 967 and anim_layer.weight ~= 0 then
+        local cycle = anim_layer.cycle
+
+        print(string.format('Reloading: %d%%', cycle * 100))
+    end
+end)
